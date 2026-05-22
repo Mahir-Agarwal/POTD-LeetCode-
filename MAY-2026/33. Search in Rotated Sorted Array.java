1f46764ce728e16e6eefeb732bccb66435f5cfa5
Mahir Aggarwal <mahir2019205@gmail.com>
2026-05-23 03:25:32 +0530

    23-05-2026

---
+++ MAY-2026/33. Search in Rotated Sorted Array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int s = 0 ;
+        int e = nums.length -1 ;
+
+
+        while (s <=e ){
+
+            int mid = (s+e) / 2;
+
+            if(nums[mid] == target){
+                return mid ; 
+                
+            } else if ( nums[s] <= nums[mid]  ){
+               
+                if(nums[s]<= target  && target < nums[mid]) e = mid-1;
+                else s = mid+1;
+
+
+            }else if (nums[e] >= nums[mid]){
+
+                if(  nums[mid] < target && nums[e] >= target  ) s= mid +1;
+                else e = mid -1; 
+
+            }
+        }
+
+
+       
+        return -1 ;
+        
+    }
+}
