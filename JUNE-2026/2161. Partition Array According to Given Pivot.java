172e7b82bbfb8723227e1585bf1e605043180b69
Mahir Aggarwal <mahir2019205@gmail.com>
2026-06-08 11:24:09 +0530

    08-06-2026

--- JUNE-2026/2161. Partition Array According to Given Pivot.java
+++ JUNE-2026/2161. Partition Array According to Given Pivot.java
@@ -22,3 +22,37 @@ class Solution {
         return res ;
     }
 }
+
+// Optimized using two pointers
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        
+        int n = nums.length;  
+        int res[] = new int[n];
+
+        int left =0 ; 
+        int right = n-1; 
+        int s =0 ;
+        int e = n-1 ; 
+
+        while ( s < n && e >=0 ) { 
+           
+           if(nums[s] < pivot) res[left++]= nums[s];
+           if(nums[e]  > pivot) res[right--]= nums[e];
+
+           s++;
+           e--;
+
+        }
+
+        while ( left <= right){
+            res[left++] = pivot;
+        }
+        System.out.println(left);
+        System.out.println(right);
+        return res ;
+    }
+}
+// [9,10,5,10,14,3,12]
+     
