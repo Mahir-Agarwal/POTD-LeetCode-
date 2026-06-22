6327e1c1cabdf566fd0a5c027fa222e592a42bba
Mahir Aggarwal <mahir2019205@gmail.com>
2026-06-22 11:03:49 +0530

    22-06-2026

---
+++ JUNE-2026/1189. Maximum Number of Balloons.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        
+        int map [] = new int [128];
+        
+        for(int i = 0  ; i < text.length() ; i++){
+            int idx  = text.charAt(i)-'a'; 
+            map[idx]++;
+        }
+    
+
+        return Math.min(map['b'-'a'] ,Math.min(map['a'-'a'] , 
+            Math.min(map['l'-'a']/2 , Math.min(map['o'-'a']/2 , map['n'-'a']))
+        ));
+    }
+}
