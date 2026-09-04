349f2786dee69b56fc94a920d19e4b9ed396c146
Mahir Aggarwal <mahir2019205@gmail.com>
2026-09-04 15:16:23 +0530

    04-09-2026

---
+++ SEP-2026/3903. Smallest Stable Index I.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int i = 1 ;
+        int j = n-2 ;
+
+        int max[] = new int [n];
+        max[0] = nums[0];
+        int min[] = new int [n];
+        min[n-1] = nums[n-1];
+
+        for(  ; i < n && j >=0 ; i++,j--){
+
+            max[i] = Math.max(nums[i], max[i-1]);
+            min[j] = Math.min(nums[j], min[j+1]);
+        }
+
+        // int res = -1 ;
+        for(i = 0 ; i < n ; i++){
+
+            int curr = max[i] - min[i] ;
+            if(curr <=k) return i;
+        }
+
+        return -1;
+
+    }
+}
+    //   i.  
+    //     j  
+// [5,0,1,4]
+// max -> 5,5 
+// min ->  , , , 4
